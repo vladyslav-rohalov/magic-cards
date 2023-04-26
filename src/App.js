e42c7b6a5c0063355baf5b5d79d374d0697a52d6
Vladyslav Rohalov <domus.lg@gmail.com>
2023-04-26 13:20:16 +0200

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { lazy } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import SharedLayout from './components/sharedLayout/sharedLayout';
 
 const Home = lazy(() => import('./pages/home/home'));
