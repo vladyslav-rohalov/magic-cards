12771c7fadd469b3a8b4df67557b6ab0b2ad63de
Vladyslav Rohalov <domus.lg@gmail.com>
2023-04-26 13:07:04 +0200

    fixed bug

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/tech-task-goit">
+      <BrowserRouter basename="/magic-cards">
         <App />
       </BrowserRouter>
     </ThemeProvider>
