66b89ff53eb4df5e172d3edfaccb5a3474978f63
Vladyslav Rohalov <domus.lg@gmail.com>
2023-04-26 12:57:53 +0200

    added strict mode

--- src/index.js
+++ src/index.js
@@ -8,11 +8,11 @@ import { theme } from './utils/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
-  <ThemeProvider theme={theme}>
-    <BrowserRouter basename="/tech-task-goit">
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename="/tech-task-goit">
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
 );
