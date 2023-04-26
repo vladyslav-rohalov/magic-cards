517b70932178a1aa61cb0e471e8bc231e8b56257
Vladyslav Rohalov <domus.lg@gmail.com>
2023-04-26 12:24:48 +0200

    Fixed bug

--- src/components/descriptionHome/descriptionHome.jsx
+++ src/components/descriptionHome/descriptionHome.jsx
@@ -7,6 +7,7 @@ export default function DescriptionStyled() {
         A small but colorful application using ReactJS and a little bit of magic
       </TextStyled>
       <iframe
+        title="animated dragon"
         id="SVGmator__0dlt1s1682432764756"
         frameBorder="0"
         src="https://www.svgmator.com/embed/0JX6ZnfyUJaIjAd?onload=true&onclick=true&onhover=true"
